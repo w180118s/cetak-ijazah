1f4786e341d9d47f4520fc6b72bf0bff6021b82e
w180118s <rahasiahidup0@gmail.com>
2021-10-25 16:42:54 +0700

    database

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "#$sdfsdfsd234g"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/db.klinik'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('dashboard.html')
 
 @app.route('/dashboard')
 def dashboard():
